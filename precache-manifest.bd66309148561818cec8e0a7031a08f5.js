12cb02381c59c0fdf2d31516a9b76708c8fa2e59
Vitaly Oryschuk <Leilyua@ukr.net>
2019-12-24 13:07:49 +0200

    Updates

---
+++ precache-manifest.bd66309148561818cec8e0a7031a08f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9301fedb17b4ad273cc64e0e99a50ce7",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "382e07c55d88796a68a2",
+    "url": "/goit-react-hw-02-reader/static/css/main.9f120a33.chunk.css"
+  },
+  {
+    "revision": "157eba8775958911609e",
+    "url": "/goit-react-hw-02-reader/static/js/2.1d21582f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-reader/static/js/2.1d21582f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "382e07c55d88796a68a2",
+    "url": "/goit-react-hw-02-reader/static/js/main.a9aad529.chunk.js"
+  },
+  {
+    "revision": "bd776cad08a94c0fa5e5",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.a3c5d2e1.js"
+  }
+]);
